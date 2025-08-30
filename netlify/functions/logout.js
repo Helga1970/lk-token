8bf4feabcc2053c8d864fc97fb92cb4390036c01
Helga1970 <olga.kostrikina@gmail.com>
2025-08-31 00:25:11 +0200

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,8 +1,13 @@
-return {
-    statusCode: 302, // Изменил статус на 302 для корректного перенаправления
-    headers: {
-        'Set-Cookie': expiredCookie,
-        'Location': '/index.html'
-    },
-    body: 'Выход выполнен успешно.'
+exports.handler = async () => {
+    // 1. Создаем переменную expiredCookie.
+    const expiredCookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT';
+
+    return {
+        statusCode: 302, // Код для перенаправления
+        headers: {
+            'Set-Cookie': expiredCookie, // 2. Используем переменную здесь
+            'Location': 'https://pro-culinaria-lk.proculinaria-book.ru' // или '/index.html'
+        },
+        body: 'Выход выполнен успешно.'
+    };
 };
