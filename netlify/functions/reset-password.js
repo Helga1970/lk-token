6d094556ed7aff35fb7669c0c09d957f6e2bad1f
Helga1970 <olga.kostrikina@gmail.com>
2025-08-30 11:39:44 +0200

    Update reset-password.js

--- netlify/functions/reset-password.js
+++ netlify/functions/reset-password.js
@@ -82,7 +82,7 @@ exports.handler = async (event) => {
             to: email,
             subject: 'Восстановление пароля',
             html: `
-                <p>Здравствуйте, **${user.name}**!</p>
+                <p>Здравствуйте, ${user.name}!</p>
                 <p>Ваш логин: ${email}</p>
                 <p>Ваш новый пароль для входа в Личный кабинет: ${newPassword}</p>
                 <p>Вы можете использовать этот пароль, чтобы войти в свой личный кабинет по этой ссылке: <a href="https://pro-culinaria-lk.proculinaria-book.ru">Войти</a></p>
