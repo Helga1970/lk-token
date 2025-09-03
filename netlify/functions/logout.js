7c3dc190429465cf1a653329d69756b68823ae7a
Helga1970 <olga.kostrikina@gmail.com>
2025-09-03 20:14:32 +0200

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,11 +1,11 @@
 exports.handler = async () => {
-    const expiredCookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=.proculinaria-book.ru; Secure; SameSite=Lax';
+    const expiredCookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=.pro-culinaria.ru; Secure; SameSite=Lax';
 
     return {
         statusCode: 302,
         headers: {
             'Set-Cookie': expiredCookie,
-            'Location': 'https://pro-culinaria-lk.proculinaria-book.ru'
+            'Location': 'https://lk.pro-culinaria.ru'
         },
         body: 'Выход выполнен успешно.'
     };
